8e7a99546c09d15e69c056f788be5675893e4dda
Deymoslor <60369872+Deymoslor@users.noreply.github.com>
2020-07-16 19:28:13 -0500

    arreglando lo dañado con la prueba

--- app/src/main/java/cursos/alain/eventually_v2/registro_Usuarios.java
+++ app/src/main/java/cursos/alain/eventually_v2/registro_Usuarios.java
@@ -1,10 +1,10 @@
 package cursos.alain.eventually_v2;
 
-import androidx.appcompat.app.AppCompatActivity;
+        import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
+        import android.content.Intent;
+        import android.os.Bundle;
+        import android.view.View;
 
 public class registro_Usuarios extends AppCompatActivity {
 
