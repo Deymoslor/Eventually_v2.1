ad440e28969bb8a32731c94a3e088e835c39b229
Deymoslor <60369872+Deymoslor@users.noreply.github.com>
2020-08-24 08:12:10 -0500

    Mostrando grupos-todos los creados por la app(personalizacion a usuarios faltando)
    En el apartado mis grupos se muestran todos los grupos, faltando asi que solo se muestren los grupos a los que pertenece el usuario, ademas de que muestre la informacion de la descripcion.

---
+++ app/src/main/java/cursos/alain/eventually_v2/Fragments/MisGruposFragment.java
@@ -0,0 +1,171 @@
+package cursos.alain.eventually_v2.Fragments;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import cursos.alain.eventually_v2.Adapters.GruposAdapter;
+import cursos.alain.eventually_v2.CreacionGrupo;
+import cursos.alain.eventually_v2.R;
+import cursos.alain.eventually_v2.entidades.Grupos1;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link MisGruposFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class MisGruposFragment extends Fragment implements Response.Listener<JSONObject>,Response.ErrorListener {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    FloatingActionButton fab;
+
+    RecyclerView recyclerGrupos;
+    ArrayList<Grupos1> listaGrupos;
+
+    ProgressDialog progress;
+
+    RequestQueue request;
+
+    JsonObjectRequest jsonObjectRequest;
+
+
+    public MisGruposFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment MisGruposFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static MisGruposFragment newInstance(String param1, String param2) {
+        MisGruposFragment fragment = new MisGruposFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View vista = inflater.inflate(R.layout.fragment_mis_grupos,container,false);
+        fab = vista.findViewById(R.id.fab_CrearGrupo);
+
+        listaGrupos = new ArrayList<>();
+
+        recyclerGrupos = (RecyclerView) vista.findViewById(R.id.idRecycler);
+        recyclerGrupos.setLayoutManager(new LinearLayoutManager(this.getContext()));
+        recyclerGrupos.setHasFixedSize(true);
+
+        request = Volley.newRequestQueue(getContext());
+
+        cargarWebservice();
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent siguiente = new Intent(getContext(), CreacionGrupo.class);
+                startActivity(siguiente);
+            }
+        });
+
+        return vista;
+    }
+
+    private void cargarWebservice() {
+
+        progress = new ProgressDialog(getContext());
+        progress.setMessage("Consultando...");
+        progress.show();
+
+        String url = "http://192.168.1.66/Eventually_01/Consultar_Grupos.php";
+
+        jsonObjectRequest = new  JsonObjectRequest(Request.Method.GET,url,null,this,this);
+        request.add(jsonObjectRequest);
+    }
+
+    @Override
+    public void onErrorResponse(VolleyError error) {
+        Toast.makeText(getContext(),"No se puedo conectar " + error.toString(), Toast.LENGTH_SHORT).show();
+        System.out.println();
+        Log.d("ERROR: ",error.toString());
+        progress.hide();
+    }
+
+    @Override
+    public void onResponse(JSONObject response) {
+        Grupos1 grupos1 = null;
+
+
+        try {
+            JSONArray json = response.optJSONArray("grupo");
+
+            for (int i = 0;i < json.length();i++){
+                grupos1 = new Grupos1();
+                JSONObject jsonObject = null;
+                jsonObject = json.getJSONObject(i);
+
+                grupos1.setNombre_Grupo(jsonObject.optString("Nombre_Grupo"));
+                grupos1.setEtiqueta(jsonObject.optString("Etiquetas"));
+                grupos1.setDescripcion(jsonObject.optString("Descripción"));
+                listaGrupos.add(grupos1);
+            }
+            progress.hide();
+            GruposAdapter adapter = new GruposAdapter(listaGrupos);
+
+            recyclerGrupos.setAdapter(adapter);
+        }
+        catch (JSONException e) {
+                e.printStackTrace();
+                Toast.makeText(getContext(),"No se pudo conectar con el servidor" + e.toString(),Toast.LENGTH_SHORT).show();
+                progress.hide();
+        }
+    }
+}
--- app/src/main/java/cursos/alain/eventually_v2/iComunicaFragments.java
+++ app/src/main/java/cursos/alain/eventually_v2/iComunicaFragments.java
@@ -1,7 +1,5 @@
 package cursos.alain.eventually_v2;
 
-import cursos.alain.eventually_v2.entidades.Grupos;
-
 public interface iComunicaFragments {
     public void enviarGrupo(Grupos grupos);
 }
